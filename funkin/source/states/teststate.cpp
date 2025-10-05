ab908995e7de21022cc6d90ac8f9377b9dc83bca
Nicholas Delatorre <58831536+NicktasticDEV@users.noreply.github.com>
2025-10-05 16:38:59 -0400

    Spritesheet test added

--- funkin/source/states/teststate.cpp
+++ funkin/source/states/teststate.cpp
@@ -1,17 +1,69 @@
 #include "teststate.h"
 
-#include "FinEngine.h"
+// Libs
 #include <grrlib.h>
+#include "FinEngine.h"
+#include <tinyxml2.h>
 #include <romfs-ogc.h>
 
-#include <wiiuse/wpad.h>
-#include <math.h>
+// OS
+#include <stdlib.h>
+#include <gccore.h>
 #include <stdio.h>
+#include <wiiuse/wpad.h>
+#include <fat.h>
+#include <math.h>
+#include <vector>
+#include <ogc/lwp_watchdog.h>
+#include <malloc.h>
 
+// Namespaces
+using namespace tinyxml2;
+
+// Initialization code
 void TestState::init() {
-    
+    printf("TestState Init\n");
+
+    lastAnimTick = gettime();
+
+    // Load font from romfs
+    font = GRRLIB_LoadTTFFromFile("romfs:/fonts/fontdef.ttf");
+    if (!font) {
+        printf("Failed to load font\n");
+    }
+
+    tex_test = GRRLIB_LoadTextureFromFile("romfs:/images/characters/boyfriend/boyfriend_main.png");
+    if (!tex_test) {
+        printf("Failed to load texture\n");
+    }
+
+    doc.LoadFile("romfs:/images/characters/boyfriend/boyfriend_main.xml");
+    if (doc.ErrorID() != 0) {
+        printf("Failed to load XML: %d\n", doc.ErrorID());
+        return;
+    }
+
+    printf("Loading done\n");
+
+    // Parse XML
+    XMLElement *root = doc.RootElement();
+    XMLElement *subtexture = root->FirstChildElement("SubTexture");
+    while (subtexture) {
+        ImgRect rect;
+        subtexture->QueryStringAttribute("name", &rect.name);
+        subtexture->QueryFloatAttribute("x", &rect.x);
+        subtexture->QueryFloatAttribute("y", &rect.y);
+        subtexture->QueryFloatAttribute("width", &rect.w);
+        subtexture->QueryFloatAttribute("height", &rect.h);
+        subtexture->QueryFloatAttribute("frameX", &rect.fx);
+        subtexture->QueryFloatAttribute("frameY", &rect.fy);
+        subtextures.push_back(rect);
+        subtexture = subtexture->NextSiblingElement("SubTexture");
+    }
+    currentRect = subtextures.empty() ? ImgRect{"",0,0,0,0} : subtextures[0];
 }
 
+// Update code
 void TestState::update() {
     WPAD_ScanPads();
 
@@ -21,17 +73,42 @@ void TestState::update() {
         finengine::FinGame::instance().Exit();
     }
 
-    
+    // Sparrow anim stuff
+    if (!subtextures.empty()) {
+        u64 now = gettime();
+        if (now - lastAnimTick >= animInterval) {
+            do {
+                animFrameIndex = (animFrameIndex + 1) % subtextures.size();
+                lastAnimTick += animInterval;
+            } while (now - lastAnimTick >= animInterval);
+            currentRect = subtextures[animFrameIndex];
+        }
+    }
 }
 
+// Render code
 void TestState::render() {
-    GRRLIB_FillScreen(0xFFFFFFFF);
+    GRRLIB_2dMode();
 
-    
+
+    GRRLIB_FillScreen(0xBABABAFF);
+    GRRLIB_PrintfTTF(0, 0, font, "Friday Night Funkin", 24, 0x000000FF);
+
+
+    GRRLIB_DrawPart(200-currentRect.fx, 100-currentRect.fy, currentRect.x, currentRect.y, currentRect.w, currentRect.h, tex_test, 0, 1, 1, 0xFFFFFFFF);
+    GRRLIB_Rectangle(200 - currentRect.fx, 100 - currentRect.fy, currentRect.w, currentRect.h, 0xFF0000FF, false);
+
+    GRRLIB_PrintfTTF(0, 450, font, currentRect.name, 24, 0x000000FF);
 
     GRRLIB_Render();
+
+    frameCount++;
 }
 
+// Cleanup code
 void TestState::cleanup() {
-    
+    GRRLIB_FreeTTF(font);
+    GRRLIB_FreeTexture(tex_test);
+    GRRLIB_Exit();
+    subtextures.clear();
 }
--- funkin/source/states/teststate.h
+++ funkin/source/states/teststate.h
@@ -2,9 +2,38 @@
 #include "FinEngine.h"
 
 #include <grrlib.h>
+#include <tinyxml2.h>
+#include <stdlib.h>
+#include <vector>
+#include <ogc/lwp_watchdog.h>
+
+using namespace tinyxml2;
 
 class TestState : public finengine::FinState {
     private:
+        struct ImgRect
+        {
+            const char* name;
+            float x;
+            float y;
+            float w;    // Width
+            float h;    // Height
+            float fx;   // Frame X
+            float fy;   // Frame Y
+            float fw;   // Frame Width
+            float fh;   // Frame Height
+        };
+
+        XMLDocument doc;
+        std::vector<ImgRect> subtextures;
+        GRRLIB_texImg *tex_test;
+        GRRLIB_ttfFont *font;
+        int frameCount = 0;
+        u64 lastAnimTick;
+        const u64 animInterval = secs_to_ticks(1) / 24;
+        size_t animFrameIndex = 0;
+        ImgRect currentRect;
+        bool loadSuccess = false;
 
     public:
         void init() override;
